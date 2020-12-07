60e7cd4cd54ff96e03f68253a6c5af51152238fb
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-07 04:07:23 -0500

    add StorePicker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// capitalize first letter of class name
+class StorePicker extends React.Component {
+  render() {
+    return <p>I am the Store Picker!</p>
+  }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-// let's go!
+// can import this because it's part of our dependencies
+// make sure second react is lower case
+import React from 'react'; 
 
-alert('hey'); 
+// importing only the render method from the 'react-dom' dependency
+import { render } from 'react-dom';
+
+// alternate 
+// import { Component } from 'react';
+// class StorePicker extends Component {}
+
+import StorePicker from './components/StorePicker.js'
+
+render(<StorePicker />, document.getElementById('main'));
