3320c5eb3144c1d65a19faa3fdd1ae82696a65b1
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-09 00:23:46 -0500

    add Order and Inventory components

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends Component {
         <div className="menu">
            <Header />
         </div>
-        {/* <Order /> */}
-        {/* <Inventory /> */}
+        <Order />
+        <Inventory />
  
       </div>
     )
---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return (
+      <div className="inventory">
+        
+      </div>
+    )
+  }
+}
+
+export default Inventory;
