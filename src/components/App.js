45c80d4c8e72e81c6b2be9d4f5d9f4fbc604f188
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-09 00:10:33 -0500

    add Header component and insert into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="catch-of-the-day">
-        <p>heyy!</p>
+        <div className="menu">
+          <Header />
+        </div>
+        <Inventory />
+        <Order />
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>Catch o the Day</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
