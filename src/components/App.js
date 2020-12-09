e9a143218eb643ea8751ad54168d6d6f0944336c
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-09 00:19:38 -0500

    add span elements to Header, create Order.js and Inventory.js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
 class App extends Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+           <Header />
         </div>
-        <Inventory />
-        <Order />
+        {/* <Order /> */}
+        {/* <Inventory /> */}
+ 
       </div>
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className="top">
-        <h1>Catch o the Day</h1>
-      </header>
+        <header className="top">
+          <h1>
+            Catch 
+            <span className="ofThe">  
+              <span className="of">of</span> 
+              <span  className="the">the</span>
+            </span> 
+            Day
+          </h1>
+          <h3 className="tagline">
+            <span>Fresh Daily</span>
+          </h3>
+        </header>
     )
   }
 }
