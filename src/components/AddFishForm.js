712443dfa6d4d38a65274d844750c91fb8d19f56
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-15 23:34:04 -0500

    add refs and fish object from ref data

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 
 class AddFishForm extends Component {
+  
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+  
+  // arrow component to allow access to 'this'
+  createFish = (e) => {
+    // stop submit action
+    e.preventDefault();
+    
+  const fish = {
+    name: this.nameRef.current.value, 
+    price: this.priceRef.current.value,
+    status: this.statusRef.current.value,
+    desc: this.descRef.current.value,
+    image: this.imageRef.current.value
+  }
+
+    console.log(this.nameRef.current.value)
+
+  }
+
   render() {
     return (
-      <form className="fish-edit">
-        <input name="name" type="text" placeholder="Name"/>
-        <input name="price" type="text" placeholder="Price"/>
-        <select name="status">
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name"/>
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price"/>
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" placeholder="Desc"></textarea>
-        <input name="imge" type="text" placeholder="Image"/>
+        <textarea name="desc" ref={this.descRef} placeholder="Desc"></textarea>
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
         <button type="submit">+ Add Fish</button>
     </form>
     )
