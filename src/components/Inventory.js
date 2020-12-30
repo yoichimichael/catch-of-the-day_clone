73741f539a10d755173990bdcf6952dc9056a965
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-30 00:44:01 -0800

    First half of 3rd party authentication
    Created Login component with login buttons for each 3rd party authenticator. Also added authenticate() function, which directs users to sign in with a selection of 3rd parties, to Inventory component and started authHandler() function to handle received authData.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
 
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import base, { firebaseApp } from '../base';
 
 class Inventory extends Component {
 
@@ -14,7 +17,27 @@ class Inventory extends Component {
     loadSampleFishes: PropTypes.func
   }  
 
+  // authData is user data from whichever platform was used to sign in
+  authHandler = async (authData) => {
+    // 1. lookup current store in firebase database
+    const store = base.fetch()
+    // 2. claim it if there's no owner
+    // 3. set the state of the inventory component to reflect the current user
+    console.log(authData)
+  }
+
+  authenticate = (provider) => {
+    // create a new auth provider based on through whom user wants to sign in
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    // user signs in with a popup and then user data gets sent to authHander()
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  }
+  
   render() {
+    return <Login authenticate={this.authenticate}/>
     return (
       <div className="inventory">
         <h2>Inventory</h2>
---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Login = props => (
+  <nav className="login">
+    <h2>Inventory Login</h2>
+    <p>Sign in to manage your store's inventory.</p>
+    <button 
+      className="github" 
+      onClick={() => props.authenticate('Github')}
+    >
+      Login With Github
+    </button>
+    <button 
+      className="twitter" 
+      onClick={() => props.authenticate('Twitter')}
+    >
+      Login With Twitter
+    </button>
+    <button 
+      className="facebook" 
+      onClick={() => props.authenticate('Facebook')}
+    >
+      Login With Facebook
+    </button>
+  </nav>
+);
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+}
+
+export default Login;
