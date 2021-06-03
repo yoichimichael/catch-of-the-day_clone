c98bc63c65da170046b71eb8d244909d4f8cb94c
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-06-02 22:39:46 -0400

    remove comments from src files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-// can import this because it's part of our dependencies
-// make sure second react is lower case
 import React from 'react'; 
 
-// alternate to above:
-// import React, { Component } from 'react';
-// class StorePicker extends Component {}
-
-// importing only the render method from the 'react-dom' dependency
 import { render } from 'react-dom';
 
-// need to use relative path
-// can leave off file extension, as .js is assumed
 import Router from './components/Router';
 
 import './css/style.css';
